20874c71f9317996e3d46b54c5aa4a96b243aa2e
Ronav <ronavrc@gmail.com>
2023-10-17 19:46:03 -0400

    Imported flask created simple API

---
+++ Server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Server supporting simple API called 'Hello'
+@app.route('/hello')
+def hello():
+    return 'hi'
+
+if __name__ == "__main__":
+    # Running server on port 5000
+    app.run(port=5000)
