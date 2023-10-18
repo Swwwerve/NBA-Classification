006d26499900523870f70f75fc941b708644957e
Ronav <ronavrc@gmail.com>
2023-10-17 21:35:46 -0400

    Added util.py methods to server

--- Server/server.py
+++ Server/server.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, jsonify
+import util
 
 app = Flask(__name__)
 
 # Server supporting simple API called 'Hello'
 # Might have to manually type in 'localhost:8000/hello' to access server 
-@app.route('/hello/')
-def hello():
-    return 'hi'
+@app.route('/classify_image/', methods=['GET','POST'])
+def classify_image(): # This function will do image classifying using saved model 
+    image_data = request.form['image_data'] # B64 encoded string
+    
+    response = jsonify(util.classify_image(image_data)) # Convert into JSON
+    
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    
+    return response
+    
 
 if __name__ == "__main__":
-    # Running server on port 5500
-    app.run(port=8000)
+    print("Starting Python Flask Server for NBA Athlete Image Classification")
+    util.load_saved_artifacts()
+    app.run(port=8000) # Running server on port 5500
