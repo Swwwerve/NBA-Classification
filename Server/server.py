423f9858d85d1cd67d03402799c3aa41637d9958
Ronav <ronavrc@gmail.com>
2023-10-17 20:17:56 -0400

    Fixed server not loading problem

--- Server/server.py
+++ Server/server.py
@@ -3,10 +3,11 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Server supporting simple API called 'Hello'
-@app.route('/hello')
+# Might have to manually type in 'localhost:8000/hello' to access server 
+@app.route('/hello/')
 def hello():
     return 'hi'
 
 if __name__ == "__main__":
-    # Running server on port 5000
-    app.run(port=5500)
+    # Running server on port 5500
+    app.run(port=8000)
