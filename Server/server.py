7ef21122dbeafb06058e7ad3f8f27ce82369e910
Ronav <ronavrc@gmail.com>
2023-10-29 14:57:55 -0400

    Before server changes

--- Server/server.py
+++ Server/server.py
@@ -1,22 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, url_for
 import util
 
 app = Flask(__name__)
 
-# Server supporting simple API called 'Hello'
-# Might have to manually type in 'localhost:8000/hello' to access server 
-@app.route('/classify_image/', methods=['GET','POST'])
-def classify_image(): # This function will do image classifying using saved model 
-    image_data = request.form['image_data'] # B64 encoded string
-    
-    response = jsonify(util.classify_image(image_data)) # Convert into JSON
-    
+@app.route('/classify_image', methods=['GET', 'POST'])
+def classify_image():
+    image_data = request.form['image_data']
+
+    response = jsonify(util.classify_image(image_data))
+
     response.headers.add('Access-Control-Allow-Origin', '*')
-    
+
     return response
-    
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server for NBA Athlete Image Classification")
+    print("Starting Python Flask Server For NBA Athlete Image Classification")
     util.load_saved_artifacts()
-    app.run(port=5500) # Running server on port 5500
+    app.run(port=5500)
